9bb34797adb17c1cb1b75d7956cc6ea54da834b3
John McGovern <eaglevision20@gmail.com>
2012-08-14 14:50:10 -0500

    Layout updates for index pages
    some work on create and update pages for Adventure Reviews

---
+++ src/HTA/HTA.Websites.MVC.FrontEnd/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTA.Adventures.Models;
+using HTA.Websites.MVC.FrontEnd.API;
+
+namespace HTA.Websites.MVC.FrontEnd.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
